8e940858264ad66d8ac8ca87886663fc9da29aac
Gajendra Sah <gajendrashah@khagendralama.com.np>
2023-04-29 13:14:26 +0545

    Font added

--- wp-content/themes/gli/header.php
+++ wp-content/themes/gli/header.php
@@ -13,6 +13,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo wp_title(); ?></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php if (is_singular() && pings_open(get_queried_object())) : ?>
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
