61ed83cb0ae8c5498b9fcefd5e3fb36bd7df90fa
Gajendra Sah <gajendrashah@khagendralama.com.np>
2023-04-29 13:11:43 +0545

    Sass setup done

---
+++ wp-content/themes/gli/static-page/tpl-components.php
@@ -0,0 +1,47 @@
+<?php
+/* Template Name: Components */
+get_header();
+?>
+
+<section class="components">
+    <div class="container">
+        <h1>Heading 1 (45px)</h1>
+        <h2>Heading 2 (40px)</h2>
+        <h3>Heading 3 (30px)</h3>
+        <h4>Heading 4 (26px)</h4>
+        <h5>Heading 5 (24px)</h5>
+        <h6>Heading 6 (18px)</h6>
+        <i class="icon-search"></i>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat assumenda laborum ea velit, vitae porro animi, reiciendis laboriosam tempora tenetur provident esse nemo quasi inventore cupiditate eos quae iusto accusantium!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat assumenda laborum ea velit, vitae porro animi, reiciendis laboriosam tempora tenetur provident esse nemo quasi inventore cupiditate eos quae iusto accusantium!</p>
+        <div>
+            <h3 class="my-1">.btn (default)</h3>
+            <a href="#" class="btn">Button</a>
+        </div>
+        <div>
+            <h3 class="my-1">.btn .btn-primary</h3>
+            <a href="#" class="btn btn-primary">Button</a>
+        </div>
+        <div>
+            <h3 class="my-1">.btn .btn-secondary</h3>
+            <a href="#" class="btn btn-secondary">Button</a>
+        </div>
+        <div>
+            <h3 class="my-1">.btn .btn-lg</h3>
+            <a href="#" class="btn btn-lg">Button</a>
+        </div>
+        <div>
+            <h3 class="my-1">.btn .btn-md</h3>
+            <a href="#" class="btn btn-md">Button</a>
+        </div>
+        <div>
+            <h3 class="my-1">.btn .btn-icon</h3>
+            <a href="#" class="btn btn-icon">Read more <i class="icon-angle-right"></i></a>
+        </div>
+        <hr>
+        <hr>
+    </div><!-- /.container -->
+</section>
+
+<?php
+get_footer();
