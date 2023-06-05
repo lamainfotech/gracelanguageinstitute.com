13376ae402962ba35f93b2c1872e0339c8179b94
Khagendra Lama <khagendralama2@gmail.com>
2023-06-05 21:00:38 +0545

    ACF Added

--- wp-content/themes/gli/header.php
+++ wp-content/themes/gli/header.php
@@ -8,10 +8,10 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta name="description" content="<?php bloginfo('description'); ?>">
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
     <title><?php echo wp_title(); ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
