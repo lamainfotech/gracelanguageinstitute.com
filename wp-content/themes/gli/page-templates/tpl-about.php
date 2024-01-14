dcbc2bed4a6d750ba70c5dfdaccef9058ac5a0c6
Khagendra Lama <khagendralama2@gmail.com>
2024-01-14 13:15:08 +0545

    padding correction

--- wp-content/themes/gli/page-templates/tpl-about.php
+++ wp-content/themes/gli/page-templates/tpl-about.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 
-<section class="about-page pt-sm-6 pt-5 pb-xxl-5 pb-lg-4 pb-sm-3 pb-2">
+<section class="about-page pt-sm-8 pt-7 pb-xxl-6 pb-lg-4 pb-sm-3 pb-2">
     <div class="container">
         <div class="block-title mt-2">
             <div class="heading-secondary text-center mb-lg-3 mb-sm-2 mb-1">
