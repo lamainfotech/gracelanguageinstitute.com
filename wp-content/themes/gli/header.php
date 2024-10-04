d6a291a71496702d70a9ce5c1127eb48ea8eb441
Khagendra Lama <khagendralama2@gmail.com>
2024-10-04 19:27:26 +0545

    Add tagline

--- wp-content/themes/gli/header.php
+++ wp-content/themes/gli/header.php
@@ -32,7 +32,10 @@
         <header class="site-header sticky">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container">
-                    <a class="navbar-brand" href="/"><img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" height="135" width="135"></a>
+                    <a class="navbar-brand" href="/">
+                        <img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>" height="135" width="135">
+                        <p class="tagline">Give language to your thoughts</p>
+                    </a>
                     <?php
                     wp_nav_menu(array(
                         'theme_location'  => 'primary',
